625d02a65209218b3a30aa833925b80e59bdc92c
Chris Curnow <chris@4cast.com.au>
2015-09-08 14:56:57 +1000

    Implement very simple list/detail for projects using Angular Route

--- 4CastLean/WebFolder/index.waPage/scripts/main.js
+++ 4CastLean/WebFolder/index.waPage/scripts/main.js
@@ -32,30 +32,32 @@
 
 
 
- var app = angular.module("myApp", ["kendo.directives", "wakanda", "ngRoute"])
+ var app = angular.module("myApp", ["kendo.directives", "wakanda", "ngRoute" 	 	])
 
 
-             app.controller("MyController", function($scope) {
+             app.controller("MyController", function($scope, $wakanda) {
 
-                $scope.greeting = "Bonjour";
-                $scope.path = "We are here";
+                $scope.greeting = "Bonjour";               
+                
+                 $wakanda.init().then(function oninit(ds) {
+                                    $scope.projects = ds.project.$find();
+                                    $scope.customOptions.dataSource = new kendo.data.DataSource({
+                                        data: $scope.projects
+                                    });
+                
+                                });
                 
             });
             
-             app.controller("projectsController", function($scope, $wakanda) {
-
-                $scope.greeting = "Bonjour";
-                $scope.path = "We are here";
-
-                $wakanda.init().then(function oninit(ds) {
-                    var projects = ds.project.$find();
-                    $scope.customOptions.dataSource = new kendo.data.DataSource({
-                        data: projects
-                    });
-
-                });
-
+             app.controller("projectsController", function($scope, $wakanda, $location) {               
+				
+				$scope.greeting = "Hello";
+								
                 $scope.customOptions = {
+                	dataSource: new kendo.data.DataSource({
+                	    data: $scope.projects
+                	}),
+                	
                     dataTextField: "projectName",
                     dataValueField: "projectNumber",
                     columns: [{
@@ -66,17 +68,36 @@
                         title: "Name"
                     }]
                 };
+                
+                
+            });
+            
+            app.controller("detailController", function($scope, $location){
+            	$scope.project = {projectName:"", projectNumber:""}
+            	
+            	$scope.save = function(){
+            		$scope.projects.push($scope.project);
+            		$location.path("/")
+            	}
+            
             });
 
-            app.config(['$routeProvider', function($routeProvider) {
+            app.config(function($routeProvider, $locationProvider) {
+            	
+            	$locationProvider.html5Mode(true);
+            
                 $routeProvider
                 	.when('/', {
-                    templateUrl: "views/projects/projectsList.html",
+                    templateUrl: "/views/projects/projectsList.html",
                     controller: "projectsController"
                     
+                	})
+                	.when('/new', {
+                		templateUrl: "/views/projects/projectDetail.html",
+                		controller: "detailController"
                 	})
                 	.otherwise({
                 		template: "<div><h2>Otherwise</h2></div>"
                 	});	
-            }]);
+            });
 
