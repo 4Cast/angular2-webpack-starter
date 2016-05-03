cafa524b33f8b3a57f836fb75e2ef7f0e9cc256a
Chris Curnow <chris@4cast.com.au>
2016-05-03 13:16:43 +1000

    Merge 4Cast Starter into clone of Angular Class Repo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,109 +1,116 @@
-
 /*
- * Angular 2
+ **************************************
+ * 4Cast Starter
+ **************************************
+ *
+ * Angular 2 decorators and services
  */
 
-import {Component, ViewEncapsulation} from 'angular2/core';
+import {Component, OnInit} from 'angular2/core';
+import {RouteConfig, Router, ROUTER_DIRECTIVES, RouterLink, OnActivate, ComponentInstruction} from 'angular2/router';
+import {FORM_PROVIDERS} from 'angular2/common';
+import {FourcastService} from '../services/fourcast-service';
 
-import {RouteConfig, Router} from 'angular2/router';
 
-
-import {Home} from './home';
-import {AppState} from './app.service';
 import {RouterActive} from './router-active';
+import {Project} from './class.definitions/project';
+import {MainMenu} from './components/main-menu/main-menu';
+import {Angular2Accordion} from './components/main-menu/main-menu-a';
 
-// Use webpack's `require` to get files as a raw string using raw-loader
+import {About} from './about';
+//import {Home} from './home/home';
+import {projects} from './components/project/projects';
+import {subcontracts} from './components/subcontracts/subcontracts';
+import {ProjectPalette} from './components/ProjectPalette/ProjectPalette';
+import {CostCodes} from './components/cost-codes/cost.codes.container';
+import {HeroDetailComponent} from './hero-detail.component';
+import {KendoGridContainer} from './components/kendo/kendo-grid-container';
+import {KendoSimple} from './components/kendo/kendo-simple';
+import {UserContainer} from './components/user/users-container';
+import {SuppliersContainer} from './components/supplier/supplier-container';
+import {SimpleCostImportSetup} from './components/costImports/simple.cost.import.setup';
+import {HeroFormComponent} from './components/form-validation/hero-form';
+import {CostCodesSetup} from './components/cost-codes/cost-code-setup/cost.codes.setup';
+
+
+//import {kendoContainer} from './components/kendo/kendo-container';
+//import {SubcontractMain} from './components/subcontracts/subcontractsMain';
+//import {ProjectsMain} from './providers/project/projectsMain'
+//import {SEMANTIC_COMPONENTS} from './directives/semantic/semantic';
+
 const APP_STYLES = require('./app.css');
+const APP_TEMPLATE = require('./app.html');
 
 /*
  * App Component
  * Top Level Component
- * Simple router component example
  */
 @Component({
 
   selector: 'app',
-  pipes: [ ],
-  providers: [ ],
-  directives: [ RouterActive ],
-  encapsulation: ViewEncapsulation.None,
-  styles: [
-    require('normalize.css'),
-    `
-    md-toolbar ul {
-      display: inline;
-      list-style-type: none;
-      margin: 0;
-      padding: 0;
-      width: 60px;
-    }
-    md-toolbar li {
-      display: inline;
-    }
-    md-toolbar li.active {
-      background-color: lightgray;
-    }
-  `],
-  template: `
-    <header>
-
-      <md-toolbar color="primary">
-        <span>{{ name }}</span>
-        <nav>
-          <ul>
-            <li router-active>
-              <a [routerLink]=" ['Index'] ">Index</a>
-            </li>
-            |
-            <li router-active>
-              <a [routerLink]=" ['Home'] ">Home</a>
-            </li>
-            |
-            <li router-active>
-              <a [routerLink]=" ['About'] ">About</a>
-            </li>
-          </ul>
-        </nav>
-      </md-toolbar>
-
-    </header>
-    <md-progress-bar mode="indeterminate" color="primary" *ngIf="loading"></md-progress-bar>
-
-    <main>
-      <router-outlet></router-outlet>
-    </main>
-
-    <pre>this.appState.state = {{ appState.state | json }}</pre>
-
-    <footer>
-
-      WebPack Angular 2 Starter by <a [href]="url">@AngularClass</a>
-      <div>
-        <img [src]="angularclassLogo" width="10%">
-      </div>
-    </footer>
-  `
+  providers: [ ...FORM_PROVIDERS, FourcastService],
+  directives: [ ...ROUTER_DIRECTIVES, RouterActive, ProjectPalette, MainMenu, Angular2Accordion ],
+  pipes: [],
+  styles: [APP_STYLES],
+  template: APP_TEMPLATE
 })
+
 @RouteConfig([
-  { path: '/',      name: 'Index', component: Home, useAsDefault: true },
-  { path: '/home',  name: 'Home',  component: Home },
-  // Async load a component using Webpack's require with es6-promise-loader and webpack `require`
-  { path: '/about', name: 'About', loader: () => require('es6-promise!./about')('About') }
+  { path: '/',            redirectTo: ['Cost-Codes'] },
+  //{ path: '/home', component: Home, name: 'Home' },
+  //{ path: '/kendo', component: kendoContainer, name: 'Kendo' },
+  { path: '/about', component: About, name: 'About' },
+  { path: '/projects/...', component: projects, name: 'Projects' },
+  { path: '/subcontracts/...', component: subcontracts, name: "Subcontracts"},
+  { path: '/users/...', component: UserContainer, name: 'Users'},
+  { path: '/suppliers/...', component: SuppliersContainer, name: 'Suppliers'},
+  {path: '/cost-codes/...', component: CostCodes, name: 'Cost-Codes'},
+  {path: '/cost-code-setup', component: CostCodesSetup, name: 'Cost-Code-Setup'},
+  { path: '/gridExample', component: KendoGridContainer, name: 'GridExample'},
+  { path: '/kendoSimple', component: KendoSimple, name: 'KendoSimple'},
+  { path: '/uploadDemo', component: SimpleCostImportSetup, name: 'ImportSetup'},
+  { path: '/form-validation', component: HeroFormComponent, name: 'HeroForm'}
+  //{ path: '/subcontract/:id/...', component: SubcontractMain, name: 'SubcontractMain' }
 ])
-export class App {
-  angularclassLogo = 'assets/img/angularclass-avatar.png';
-  loading = false;
-  name = 'Angular 2 Webpack Starter';
-  url = 'https://twitter.com/AngularClass';
+export class App implements OnInit  {
+  url: string = 'https://twitter.com/AngularClass';
+  public name = '4Cast Project Cost Control';
+  public projectName = 'Project 110001'
+  public theProject: Object;
 
-  constructor(
-    public appState: AppState) {
+  constructor(public fourcast: FourcastService) {
 
   }
 
-  ngOnInit() {
-    console.log('Initial App State', this.appState.state);
+  ngOnInit(){
+
+    this.fourcast.initProject()
+    .subscribe((res)=>{
+      this.theProject = res;
+    })
+
   }
+
+
+  // following has some interesting stuff from upstream master
+  // leave it here as is in comments for later reference.
+  // =======
+  //   { path: '/', component: Home, name: 'Index' },
+  //   { path: '/home', component: Home, name: 'Home' },
+  //   // Async load a component using Webpack's require with es6-promise-loader
+  //   { path: '/about', loader: () => require('./about/about')('About'), name: 'About' },
+  //   { path: '/**', redirectTo: ['Index'] }
+  // ])
+  // export class App {
+  //   angularclassLogo = 'assets/img/angularclass-avatar.png';
+  //   name = 'Angular 2 Webpack Starter';
+  //   url = 'https://twitter.com/AngularClass';
+  //   constructor() {
+  //
+  //   }
+  // >>>>>>> upstream/master
+
+
 
 }
 
@@ -113,4 +120,5 @@ export class App {
  * (The examples may not be updated as quickly. Please open an issue on github for us to update it)
  * For help or questions please contact us at @AngularClass on twitter
  * or our chat on Slack at https://AngularClass.com/slack-join
+ * or via chat on Gitter at https://gitter.im/AngularClass/angular2-webpack-starter
  */
